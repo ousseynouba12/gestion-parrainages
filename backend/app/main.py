33cf33605dde229c82a58e8868b853d6347f0ecc
ousseynouba12 <168310009+ousseynouba12@users.noreply.github.com>
2025-02-11 11:59:44 +0000

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.api.routes import users 
 app = FastAPI()
 
+app.include_router(users.router, prefix="/users", tags=["Users"])
+
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
