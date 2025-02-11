40d2e35b900435e0132265ff704da41ace9bed21
ousseynouba12 <168310009+ousseynouba12@users.noreply.github.com>
2025-02-11 12:37:26 +0000

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from app.api.routes import users 
+
 app = FastAPI()
-print("App is starting...")
+
 app.include_router(users.router, prefix="/users", tags=["Users"])
 
 @app.get("/")
