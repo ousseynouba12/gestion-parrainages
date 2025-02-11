ee6a0a4d8251ef2ed16052fb28bd310468408c5d
ousseynouba12 <168310009+ousseynouba12@users.noreply.github.com>
2025-02-11 12:57:57 +0000

    Update users.py

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -4,10 +4,6 @@ from app.db.database import get_db
 
 router = APIRouter()
 
-@router.get("/test-db")
-def test_db(db: Session = Depends(get_db)):
-    try:
-        db.execute("SELECT 1")  # Test de la connexion à la base de données
-        return {"message": "Connexion réussie ! 🎉"}
-    except Exception as e:
-        return {"error": str(e)}
+@router.get("/hello")
+def test_route():
+    return {"message": "Route works!"}
