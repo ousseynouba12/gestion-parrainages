949252d0eb022a81df5ead4ab29c6af8ea8acfeb
papaousseynouba@esp.sn <papaousseynouba@esp.sn>
2025-03-05 11:48:22 +0000

    setup cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints import router as api_router
 from app.core.config import settings
+from app.core.middleware import setup_middlewares
 
 # Initialisation de l'application FastAPI
 app = FastAPI(
@@ -9,7 +10,7 @@ app = FastAPI(
     description="API pour la gestion des parrainages aux élections présidentielles au Sénégal.",
     version="1.0.0"
 )
-
+setup_middlewares(app)
 
 # Inclusion des routes
 app.include_router(api_router, prefix="/api/v1")
